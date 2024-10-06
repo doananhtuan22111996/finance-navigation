77e7632bd77d898502fb0c9381c6091decdae323
Jason Doan <35907749+doananhtuan22111996@users.noreply.github.com>
2024-10-06 23:43:09 +0700

    refactor: Navigation manager (#4)

--- navigation/src/main/java/vn/finance/navigation/NavigationKey.kt
+++ navigation/src/main/java/vn/finance/navigation/NavigationKey.kt
@@ -1,8 +1,10 @@
 package vn.finance.navigation
 
-sealed class NavigationKey {
+sealed class NavigationKey(open val key: String) {
 
-    data class Onboarding(val key: String = "onboarding") : NavigationKey()
-    data class Authentication(val key: String = "authentication") : NavigationKey()
-    data class Home(val key: String = "home") : NavigationKey()
+    data class Onboarding(override val key: String = "onboarding") : NavigationKey(key = key)
+    data class Authentication(override val key: String = "authentication") :
+        NavigationKey(key = key)
+
+    data class Home(override val key: String = "home") : NavigationKey(key = key)
 }
